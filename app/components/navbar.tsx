dae18954c3a43d8e13f6a0a976b4ffcf8e392a91
Raynaldi <raysnonci@gmail.com>
2023-08-21 08:12:26 +0700

    fix navbar css

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,21 @@
+"use client";
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathName = usePathname();
+  const isActive = (path: any) => pathName === path;
+  
+
   return (
-    <div>
-        | 
-        <Link href='/'>Home</Link> | 
-        <Link href='/about'>About</Link> | 
-        <Link href='/about/us'>About Us</Link> | 
-        <Link href='/about/someone'>About Someone Else</Link> |
-        <Link href='/listofposts'>Posts</Link> |
+    <div className={styles.navbar}>
+      <Link href='/' className={isActive('/') ? styles.activeLink : styles.navLink}>Home</Link>
+      <Link href='/about' className={isActive('/about') ? styles.activeLink : styles.navLink}>About</Link>
+      <Link href='/about/us' className={isActive('/about/us') ? styles.activeLink : styles.navLink}>About Us</Link>
+      <Link href='/about/someone' className={isActive('/about/someone') ? styles.activeLink : styles.navLink}>About Someone Else</Link>
+      <Link href='/listofposts' className={isActive('/listofposts') ? styles.activeLink : styles.navLink}>Posts</Link>
     </div>
   )
 }
